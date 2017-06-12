364d2868a81e2df1a0667a28f7a846c686f7fcb3
PerrineDeBrabant <perrine.de.brabant@student.howest.be>
2017-06-12 11:03:21 +0200

    Flask toegepast op html

--- SmartFish.py
+++ SmartFish.py
@@ -1,12 +1,47 @@
 from flask import Flask
+from flask import render_template
+
+from DbClass import DbClass
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/home')
+def home():
+    return render_template('index.html')
 
+@app.route('/')
+def onboarding():
+    return render_template('Onboarding.html')
+
+@app.route('/registreren')
+def registreren():
+    return render_template('Registreren.html')
+
+@app.route('/aanmelden')
+def aanmelden():
+    return render_template('Aanmelden.html')
+
+@app.route('/aquariums')
+def aquariums():
+    return render_template('Aquariums.html')
+
+@app.route('/filter')
+def filter():
+    return render_template('Filter.html')
+
+@app.route('/temperatuur')
+def temperatuur():
+    return render_template('Temperatuur.html')
+
+@app.route('/verlichting')
+def verlichting():
+    return render_template('Verlichting.html')
+
+@app.route('/vissen')
+def vissen():
+    return render_template('Vissen.html')
 
 if __name__ == '__main__':
     app.run()
+
